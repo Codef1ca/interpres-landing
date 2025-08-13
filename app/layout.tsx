dbc372c65be8cec80f90aaaa5948a25ff07db970
codef1ca <ramospaula@gmail.com>
2025-08-13 18:02:37 -0300

    pixelmeta

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Carousel from "@/components/carrusel";
 import Image from "next/image";
 import { Providers } from "./providers"; 
 import Link from "next/link";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -14,7 +15,39 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
+       <head>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s) {
+              if(f.fbq) return;
+              n=f.fbq=function() {
+                n.callMethod ?
+                  n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+              };
+              if(!f._fbq) f._fbq=n;
+              n.push=n; n.loaded=!0; n.version='2.0';
+              n.queue=[];
+              t=b.createElement(e); t.async=!0;
+              t.src=v; s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2357827661277787');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* Noscript para navegadores sin JS */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2357827661277787&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+      </head>
       <body>
         <Providers>
           <div>
